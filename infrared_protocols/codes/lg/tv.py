560688c7867d06c1e1c514cb71690d23e382a8ad
abmantis <amfcalt@gmail.com>
2026-03-02 17:37:18 +0000

    Address PR review comments on LG TV codes
    - Use relative import in codes/lg/tv.py (from ...commands)
    - Truncate LGTVCode enum values to 8-bit command bytes (drop redundant inverted byte)
    - Re-export LGTVCode and make_lg_tv_command from package __init__.py

--- infrared_protocols/codes/lg/tv.py
+++ infrared_protocols/codes/lg/tv.py
@@ -2,51 +2,51 @@
 
 from enum import IntEnum
 
-from infrared_protocols.commands import Command, NECCommand
+from ...commands import Command, NECCommand
 
 
 class LGTVCode(IntEnum):
     """LG TV IR command codes."""
 
-    BACK = 0xD728
-    CHANNEL_DOWN = 0xFE01
-    CHANNEL_UP = 0xFF00
-    EXIT = 0xA45B
-    FAST_FORWARD = 0x718E
-    GUIDE = 0x56A9
-    HDMI_1 = 0x31CE
-    HDMI_2 = 0x33CC
-    HDMI_3 = 0x16E9
-    HDMI_4 = 0x25DA
-    HOME = 0x837C
-    INFO = 0x55AA
-    INPUT = 0xF40B
-    MENU = 0xBC43
-    MUTE = 0xF609
-    NAV_DOWN = 0xBE41
-    NAV_LEFT = 0xF807
-    NAV_RIGHT = 0xF906
-    NAV_UP = 0xBF40
-    NUM_0 = 0xEF10
-    NUM_1 = 0xEE11
-    NUM_2 = 0xED12
-    NUM_3 = 0xEC13
-    NUM_4 = 0xEB14
-    NUM_5 = 0xEA15
-    NUM_6 = 0xE916
-    NUM_7 = 0xE817
-    NUM_8 = 0xE718
-    NUM_9 = 0xE619
-    OK = 0xBB44
-    PAUSE = 0x45BA
-    PLAY = 0x4FB0
-    POWER = 0xF708
-    POWER_ON = 0x3BC4
-    POWER_OFF = 0x3AC5
-    REWIND = 0x708F
-    STOP = 0x4EB1
-    VOLUME_DOWN = 0xFC03
-    VOLUME_UP = 0xFD02
+    BACK = 0x28
+    CHANNEL_DOWN = 0x01
+    CHANNEL_UP = 0x00
+    EXIT = 0x5B
+    FAST_FORWARD = 0x8E
+    GUIDE = 0xA9
+    HDMI_1 = 0xCE
+    HDMI_2 = 0xCC
+    HDMI_3 = 0xE9
+    HDMI_4 = 0xDA
+    HOME = 0x7C
+    INFO = 0xAA
+    INPUT = 0x0B
+    MENU = 0x43
+    MUTE = 0x09
+    NAV_DOWN = 0x41
+    NAV_LEFT = 0x07
+    NAV_RIGHT = 0x06
+    NAV_UP = 0x40
+    NUM_0 = 0x10
+    NUM_1 = 0x11
+    NUM_2 = 0x12
+    NUM_3 = 0x13
+    NUM_4 = 0x14
+    NUM_5 = 0x15
+    NUM_6 = 0x16
+    NUM_7 = 0x17
+    NUM_8 = 0x18
+    NUM_9 = 0x19
+    OK = 0x44
+    PAUSE = 0xBA
+    PLAY = 0xB0
+    POWER = 0x08
+    POWER_ON = 0xC4
+    POWER_OFF = 0xC5
+    REWIND = 0x8F
+    STOP = 0xB1
+    VOLUME_DOWN = 0x03
+    VOLUME_UP = 0x02
 
 
 def make_command(code: LGTVCode, repeat_count: int = 0) -> Command:
