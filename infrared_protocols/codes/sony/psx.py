65db4c7f00449ad66dd057df295d3c61b2317374
Nogic <24802730+nogic1008@users.noreply.github.com>
2026-05-15 21:10:02 +0900

    fix: change to SonyPSXCode enum class

--- infrared_protocols/codes/sony/psx.py
+++ infrared_protocols/codes/sony/psx.py
@@ -21,293 +21,94 @@ from enum import Enum
 from ...commands import Command
 from ...commands.sony import SonyCommand
 
-_ADDRESS_SWITCH_1 = 0x1A93
-_ADDRESS_SWITCH_2 = 0x1A9B
-_ADDRESS_SWITCH_3 = 0x1AA3
-
-# region Command code values
-_CMD_1 = 0
-_CMD_2 = 1
-_CMD_3 = 2
-_CMD_4 = 3
-_CMD_5 = 4
-_CMD_6 = 5
-_CMD_7 = 6
-_CMD_8 = 7
-_CMD_9 = 8
-_CMD_10 = 9
-_CMD_11 = 10
-_CMD_12 = 11
-_CMD_BS_7 = 13  # RMT-P001 only
-_CMD_BS_11 = 14  # RMT-P001 only
-_CMD_CLEAR = 15
-_CMD_POWER = 21
-_CMD_EJECT = 22
-_CMD_STOP = 24
-_CMD_PAUSE = 25
-_CMD_PLAY = 26
-_CMD_RECORD_START = 29
-_CMD_RECORD_PAUSE = 30
-_CMD_RECORD_STOP = 31
-_CMD_DISPLAY = 37
-_CMD_RECORDING_MODE = 38
-_CMD_MENU = 41
-_CMD_PROGRAM = 42
-_CMD_TOP_MENU = 44
-_CMD_G_GUIDE2 = 65  # RMT-P002J only
-_CMD_HOME = 66
-_CMD_RETURN = 67
-_CMD_G_GUIDE = 69  # RMT-P001 only
-_CMD_SELECT = 80
-_CMD_L3 = 81
-_CMD_R3 = 82
-_CMD_START = 83
-_CMD_UP = 84
-_CMD_RIGHT = 85
-_CMD_DOWN = 86
-_CMD_LEFT = 87
-_CMD_L2_SCAN_BACK = 88
-_CMD_R2_SCAN_FORW = 89
-_CMD_L1_PREV = 90
-_CMD_R1_NEXT = 91
-_CMD_TRIANGLE_OPTION = 92
-_CMD_CIRCLE = 93
-_CMD_CROSS_BACK = 94
-_CMD_SQUARE_VIEW = 95
-_CMD_ENTER = 96
-_CMD_QUIT_GAME = 97
-_CMD_DELETE = 98
-_CMD_FLASH_FORW = 117  # RMT-P002J only
-_CMD_FLASH_BACK = 118  # RMT-P002J only
-# endregion Command code values
-
-# Code grouping by remote model
-_P001_ONLY_COMMANDS = frozenset(
-    [
-        _CMD_G_GUIDE,
-        _CMD_BS_7,
-        _CMD_BS_11,
-    ]
-)
-
-_P002J_ONLY_COMMANDS = frozenset(
-    [
-        _CMD_G_GUIDE2,
-        _CMD_FLASH_FORW,
-        _CMD_FLASH_BACK,
-    ]
-)
+_SWITCH_TO_ADDRESS = {
+    1: 0x1A93,
+    2: 0x1A9B,
+    3: 0x1AA3,
+}
 
 
-class _PSXSwitchCodeMixin:
-    """Non-Enum mixin providing to_command() for PSX DIP-switch code enums.
+class SonyPSXCode(Enum):
+    """Sony PSX IR command codes.
 
-    Values in the concrete Enum subclasses are (address, command) pairs.
+    All codes use 20-bit SONY SIRC (address_bits=13).
+
+    address:
+    - 0x1A93: switch position 1
+    - 0x1A9B: switch position 2
+    - 0x1AA3: switch position 3
     """
 
-    def to_command(self) -> Command:
-        """Build a SONY SIRC command for this PSX code."""
-        address, command = self.value  # type: ignore[attr-defined]
+    NUM_1 = 0
+    NUM_2 = 1
+    NUM_3 = 2
+    NUM_4 = 3
+    NUM_5 = 4
+    NUM_6 = 5
+    NUM_7 = 6
+    NUM_8 = 7
+    NUM_9 = 8
+    NUM_10 = 9
+    NUM_11 = 10
+    NUM_12 = 11
+    BS_7 = 13  # RMT-P001 only
+    BS_11 = 14  # RMT-P001 only
+    CLEAR = 15
+    POWER = 21
+    EJECT = 22
+    STOP = 24
+    PAUSE = 25
+    PLAY = 26
+    RECORD_START = 29
+    RECORD_PAUSE = 30
+    RECORD_STOP = 31
+    DISPLAY = 37
+    RECORDING_MODE = 38
+    MENU = 41
+    PROGRAM = 42
+    TOP_MENU = 44
+    G_GUIDE2 = 65  # RMT-P002J only
+    HOME = 66
+    RETURN = 67
+    G_GUIDE = 69  # RMT-P001 only
+    SELECT = 80
+    L3 = 81
+    R3 = 82
+    START = 83
+    UP = 84
+    RIGHT = 85
+    DOWN = 86
+    LEFT = 87
+    L2_SCAN_BACK = 88
+    R2_SCAN_FORW = 89
+    L1_PREV = 90
+    R1_NEXT = 91
+    TRIANGLE_OPTION = 92
+    CIRCLE = 93
+    CROSS_BACK = 94
+    SQUARE_VIEW = 95
+    ENTER = 96
+    QUIT_GAME = 97
+    DELETE = 98
+    FLASH_FORW = 117  # RMT-P002J only
+    FLASH_BACK = 118  # RMT-P002J only
+
+    def to_command(self, switch: int = 1) -> Command:
+        """Build a SONY SIRC command for this PSX code.
+
+        :param switch: The switch position on the PSX remote receiver unit (1, 2, or 3).
+            Each position maps to a distinct IR address, allowing multiple PSX units
+            to be controlled independently in the same room. Defaults to 1.
+        """
+        if switch not in _SWITCH_TO_ADDRESS.keys():
+            raise ValueError(f"Invalid switch position: {switch}")
+
+        command = self.value
         return SonyCommand(
-            address=address,
+            address=_SWITCH_TO_ADDRESS[switch],
             address_bits=13,
             command=command,
         )
-
-
-class SonyPSXSwitch1Code(_PSXSwitchCodeMixin, Enum):
-    """Sony PSX (remote switch 1) IR command codes.
-
-    All codes use 20-bit SONY SIRC (address_bits=13).
-
-    address is 0x1A93 for switch position 1.
-
-    Values are (address, command) pairs.
-    """
-
-    NUM_1 = (_ADDRESS_SWITCH_1, _CMD_1)
-    NUM_2 = (_ADDRESS_SWITCH_1, _CMD_2)
-    NUM_3 = (_ADDRESS_SWITCH_1, _CMD_3)
-    NUM_4 = (_ADDRESS_SWITCH_1, _CMD_4)
-    NUM_5 = (_ADDRESS_SWITCH_1, _CMD_5)
-    NUM_6 = (_ADDRESS_SWITCH_1, _CMD_6)
-    NUM_7 = (_ADDRESS_SWITCH_1, _CMD_7)
-    NUM_8 = (_ADDRESS_SWITCH_1, _CMD_8)
-    NUM_9 = (_ADDRESS_SWITCH_1, _CMD_9)
-    NUM_10 = (_ADDRESS_SWITCH_1, _CMD_10)
-    NUM_11 = (_ADDRESS_SWITCH_1, _CMD_11)
-    NUM_12 = (_ADDRESS_SWITCH_1, _CMD_12)
-    BS_7 = (_ADDRESS_SWITCH_1, _CMD_BS_7)
-    BS_11 = (_ADDRESS_SWITCH_1, _CMD_BS_11)
-    CLEAR = (_ADDRESS_SWITCH_1, _CMD_CLEAR)
-    POWER = (_ADDRESS_SWITCH_1, _CMD_POWER)
-    EJECT = (_ADDRESS_SWITCH_1, _CMD_EJECT)
-    STOP = (_ADDRESS_SWITCH_1, _CMD_STOP)
-    PAUSE = (_ADDRESS_SWITCH_1, _CMD_PAUSE)
-    PLAY = (_ADDRESS_SWITCH_1, _CMD_PLAY)
-    RECORD_START = (_ADDRESS_SWITCH_1, _CMD_RECORD_START)
-    RECORD_PAUSE = (_ADDRESS_SWITCH_1, _CMD_RECORD_PAUSE)
-    RECORD_STOP = (_ADDRESS_SWITCH_1, _CMD_RECORD_STOP)
-    DISPLAY = (_ADDRESS_SWITCH_1, _CMD_DISPLAY)
-    RECORDING_MODE = (_ADDRESS_SWITCH_1, _CMD_RECORDING_MODE)
-    MENU = (_ADDRESS_SWITCH_1, _CMD_MENU)
-    PROGRAM = (_ADDRESS_SWITCH_1, _CMD_PROGRAM)
-    TOP_MENU = (_ADDRESS_SWITCH_1, _CMD_TOP_MENU)
-    G_GUIDE2 = (_ADDRESS_SWITCH_1, _CMD_G_GUIDE2)
-    HOME = (_ADDRESS_SWITCH_1, _CMD_HOME)
-    RETURN = (_ADDRESS_SWITCH_1, _CMD_RETURN)
-    G_GUIDE = (_ADDRESS_SWITCH_1, _CMD_G_GUIDE)
-    SELECT = (_ADDRESS_SWITCH_1, _CMD_SELECT)
-    L3 = (_ADDRESS_SWITCH_1, _CMD_L3)
-    R3 = (_ADDRESS_SWITCH_1, _CMD_R3)
-    START = (_ADDRESS_SWITCH_1, _CMD_START)
-    UP = (_ADDRESS_SWITCH_1, _CMD_UP)
-    RIGHT = (_ADDRESS_SWITCH_1, _CMD_RIGHT)
-    DOWN = (_ADDRESS_SWITCH_1, _CMD_DOWN)
-    LEFT = (_ADDRESS_SWITCH_1, _CMD_LEFT)
-    L2_SCAN_BACK = (_ADDRESS_SWITCH_1, _CMD_L2_SCAN_BACK)
-    R2_SCAN_FORW = (_ADDRESS_SWITCH_1, _CMD_R2_SCAN_FORW)
-    L1_PREV = (_ADDRESS_SWITCH_1, _CMD_L1_PREV)
-    R1_NEXT = (_ADDRESS_SWITCH_1, _CMD_R1_NEXT)
-    TRIANGLE_OPTION = (_ADDRESS_SWITCH_1, _CMD_TRIANGLE_OPTION)
-    CIRCLE = (_ADDRESS_SWITCH_1, _CMD_CIRCLE)
-    CROSS_BACK = (_ADDRESS_SWITCH_1, _CMD_CROSS_BACK)
-    SQUARE_VIEW = (_ADDRESS_SWITCH_1, _CMD_SQUARE_VIEW)
-    ENTER = (_ADDRESS_SWITCH_1, _CMD_ENTER)
-    QUIT_GAME = (_ADDRESS_SWITCH_1, _CMD_QUIT_GAME)
-    DELETE = (_ADDRESS_SWITCH_1, _CMD_DELETE)
-    FLASH_FORW = (_ADDRESS_SWITCH_1, _CMD_FLASH_FORW)
-    FLASH_BACK = (_ADDRESS_SWITCH_1, _CMD_FLASH_BACK)
-
-
-class SonyPSXSwitch2Code(_PSXSwitchCodeMixin, Enum):
-    """Sony PSX (remote switch 2) IR command codes.
-
-    All codes use 20-bit SONY SIRC (address_bits=13).
-
-    address is 0x1A9B for switch position 2.
-
-    Values are (address, command) pairs.
-    """
-
-    NUM_1 = (_ADDRESS_SWITCH_2, _CMD_1)
-    NUM_2 = (_ADDRESS_SWITCH_2, _CMD_2)
-    NUM_3 = (_ADDRESS_SWITCH_2, _CMD_3)
-    NUM_4 = (_ADDRESS_SWITCH_2, _CMD_4)
-    NUM_5 = (_ADDRESS_SWITCH_2, _CMD_5)
-    NUM_6 = (_ADDRESS_SWITCH_2, _CMD_6)
-    NUM_7 = (_ADDRESS_SWITCH_2, _CMD_7)
-    NUM_8 = (_ADDRESS_SWITCH_2, _CMD_8)
-    NUM_9 = (_ADDRESS_SWITCH_2, _CMD_9)
-    NUM_10 = (_ADDRESS_SWITCH_2, _CMD_10)
-    NUM_11 = (_ADDRESS_SWITCH_2, _CMD_11)
-    NUM_12 = (_ADDRESS_SWITCH_2, _CMD_12)
-    BS_7 = (_ADDRESS_SWITCH_2, _CMD_BS_7)
-    BS_11 = (_ADDRESS_SWITCH_2, _CMD_BS_11)
-    CLEAR = (_ADDRESS_SWITCH_2, _CMD_CLEAR)
-    POWER = (_ADDRESS_SWITCH_2, _CMD_POWER)
-    EJECT = (_ADDRESS_SWITCH_2, _CMD_EJECT)
-    STOP = (_ADDRESS_SWITCH_2, _CMD_STOP)
-    PAUSE = (_ADDRESS_SWITCH_2, _CMD_PAUSE)
-    PLAY = (_ADDRESS_SWITCH_2, _CMD_PLAY)
-    RECORD_START = (_ADDRESS_SWITCH_2, _CMD_RECORD_START)
-    RECORD_PAUSE = (_ADDRESS_SWITCH_2, _CMD_RECORD_PAUSE)
-    RECORD_STOP = (_ADDRESS_SWITCH_2, _CMD_RECORD_STOP)
-    DISPLAY = (_ADDRESS_SWITCH_2, _CMD_DISPLAY)
-    RECORDING_MODE = (_ADDRESS_SWITCH_2, _CMD_RECORDING_MODE)
-    MENU = (_ADDRESS_SWITCH_2, _CMD_MENU)
-    PROGRAM = (_ADDRESS_SWITCH_2, _CMD_PROGRAM)
-    TOP_MENU = (_ADDRESS_SWITCH_2, _CMD_TOP_MENU)
-    G_GUIDE2 = (_ADDRESS_SWITCH_2, _CMD_G_GUIDE2)
-    HOME = (_ADDRESS_SWITCH_2, _CMD_HOME)
-    RETURN = (_ADDRESS_SWITCH_2, _CMD_RETURN)
-    G_GUIDE = (_ADDRESS_SWITCH_2, _CMD_G_GUIDE)
-    SELECT = (_ADDRESS_SWITCH_2, _CMD_SELECT)
-    L3 = (_ADDRESS_SWITCH_2, _CMD_L3)
-    R3 = (_ADDRESS_SWITCH_2, _CMD_R3)
-    START = (_ADDRESS_SWITCH_2, _CMD_START)
-    UP = (_ADDRESS_SWITCH_2, _CMD_UP)
-    RIGHT = (_ADDRESS_SWITCH_2, _CMD_RIGHT)
-    DOWN = (_ADDRESS_SWITCH_2, _CMD_DOWN)
-    LEFT = (_ADDRESS_SWITCH_2, _CMD_LEFT)
-    L2_SCAN_BACK = (_ADDRESS_SWITCH_2, _CMD_L2_SCAN_BACK)
-    R2_SCAN_FORW = (_ADDRESS_SWITCH_2, _CMD_R2_SCAN_FORW)
-    L1_PREV = (_ADDRESS_SWITCH_2, _CMD_L1_PREV)
-    R1_NEXT = (_ADDRESS_SWITCH_2, _CMD_R1_NEXT)
-    TRIANGLE_OPTION = (_ADDRESS_SWITCH_2, _CMD_TRIANGLE_OPTION)
-    CIRCLE = (_ADDRESS_SWITCH_2, _CMD_CIRCLE)
-    CROSS_BACK = (_ADDRESS_SWITCH_2, _CMD_CROSS_BACK)
-    SQUARE_VIEW = (_ADDRESS_SWITCH_2, _CMD_SQUARE_VIEW)
-    ENTER = (_ADDRESS_SWITCH_2, _CMD_ENTER)
-    QUIT_GAME = (_ADDRESS_SWITCH_2, _CMD_QUIT_GAME)
-    DELETE = (_ADDRESS_SWITCH_2, _CMD_DELETE)
-    FLASH_FORW = (_ADDRESS_SWITCH_2, _CMD_FLASH_FORW)
-    FLASH_BACK = (_ADDRESS_SWITCH_2, _CMD_FLASH_BACK)
-
-
-class SonyPSXSwitch3Code(_PSXSwitchCodeMixin, Enum):
-    """Sony PSX (remote switch 3) IR command codes.
-
-    All codes use 20-bit SONY SIRC (address_bits=13).
-
-    address is 0x1AA3 for switch position 3.
-
-    Values are (address, command) pairs.
-    """
-
-    NUM_1 = (_ADDRESS_SWITCH_3, _CMD_1)
-    NUM_2 = (_ADDRESS_SWITCH_3, _CMD_2)
-    NUM_3 = (_ADDRESS_SWITCH_3, _CMD_3)
-    NUM_4 = (_ADDRESS_SWITCH_3, _CMD_4)
-    NUM_5 = (_ADDRESS_SWITCH_3, _CMD_5)
-    NUM_6 = (_ADDRESS_SWITCH_3, _CMD_6)
-    NUM_7 = (_ADDRESS_SWITCH_3, _CMD_7)
-    NUM_8 = (_ADDRESS_SWITCH_3, _CMD_8)
-    NUM_9 = (_ADDRESS_SWITCH_3, _CMD_9)
-    NUM_10 = (_ADDRESS_SWITCH_3, _CMD_10)
-    NUM_11 = (_ADDRESS_SWITCH_3, _CMD_11)
-    NUM_12 = (_ADDRESS_SWITCH_3, _CMD_12)
-    BS_7 = (_ADDRESS_SWITCH_3, _CMD_BS_7)
-    BS_11 = (_ADDRESS_SWITCH_3, _CMD_BS_11)
-    CLEAR = (_ADDRESS_SWITCH_3, _CMD_CLEAR)
-    POWER = (_ADDRESS_SWITCH_3, _CMD_POWER)
-    EJECT = (_ADDRESS_SWITCH_3, _CMD_EJECT)
-    STOP = (_ADDRESS_SWITCH_3, _CMD_STOP)
-    PAUSE = (_ADDRESS_SWITCH_3, _CMD_PAUSE)
-    PLAY = (_ADDRESS_SWITCH_3, _CMD_PLAY)
-    RECORD_START = (_ADDRESS_SWITCH_3, _CMD_RECORD_START)
-    RECORD_PAUSE = (_ADDRESS_SWITCH_3, _CMD_RECORD_PAUSE)
-    RECORD_STOP = (_ADDRESS_SWITCH_3, _CMD_RECORD_STOP)
-    DISPLAY = (_ADDRESS_SWITCH_3, _CMD_DISPLAY)
-    RECORDING_MODE = (_ADDRESS_SWITCH_3, _CMD_RECORDING_MODE)
-    MENU = (_ADDRESS_SWITCH_3, _CMD_MENU)
-    PROGRAM = (_ADDRESS_SWITCH_3, _CMD_PROGRAM)
-    TOP_MENU = (_ADDRESS_SWITCH_3, _CMD_TOP_MENU)
-    G_GUIDE2 = (_ADDRESS_SWITCH_3, _CMD_G_GUIDE2)
-    HOME = (_ADDRESS_SWITCH_3, _CMD_HOME)
-    RETURN = (_ADDRESS_SWITCH_3, _CMD_RETURN)
-    G_GUIDE = (_ADDRESS_SWITCH_3, _CMD_G_GUIDE)
-    SELECT = (_ADDRESS_SWITCH_3, _CMD_SELECT)
-    L3 = (_ADDRESS_SWITCH_3, _CMD_L3)
-    R3 = (_ADDRESS_SWITCH_3, _CMD_R3)
-    START = (_ADDRESS_SWITCH_3, _CMD_START)
-    UP = (_ADDRESS_SWITCH_3, _CMD_UP)
-    RIGHT = (_ADDRESS_SWITCH_3, _CMD_RIGHT)
-    DOWN = (_ADDRESS_SWITCH_3, _CMD_DOWN)
-    LEFT = (_ADDRESS_SWITCH_3, _CMD_LEFT)
-    L2_SCAN_BACK = (_ADDRESS_SWITCH_3, _CMD_L2_SCAN_BACK)
-    R2_SCAN_FORW = (_ADDRESS_SWITCH_3, _CMD_R2_SCAN_FORW)
-    L1_PREV = (_ADDRESS_SWITCH_3, _CMD_L1_PREV)
-    R1_NEXT = (_ADDRESS_SWITCH_3, _CMD_R1_NEXT)
-    TRIANGLE_OPTION = (_ADDRESS_SWITCH_3, _CMD_TRIANGLE_OPTION)
-    CIRCLE = (_ADDRESS_SWITCH_3, _CMD_CIRCLE)
-    CROSS_BACK = (_ADDRESS_SWITCH_3, _CMD_CROSS_BACK)
-    SQUARE_VIEW = (_ADDRESS_SWITCH_3, _CMD_SQUARE_VIEW)
-    ENTER = (_ADDRESS_SWITCH_3, _CMD_ENTER)
-    QUIT_GAME = (_ADDRESS_SWITCH_3, _CMD_QUIT_GAME)
-    DELETE = (_ADDRESS_SWITCH_3, _CMD_DELETE)
-    FLASH_FORW = (_ADDRESS_SWITCH_3, _CMD_FLASH_FORW)
-    FLASH_BACK = (_ADDRESS_SWITCH_3, _CMD_FLASH_BACK)
 
 
 @dataclass(frozen=True, slots=True)
@@ -318,72 +119,36 @@ class PSXModel:
     codes: frozenset[Enum]
 
 
+# Code grouping by remote model
+_P001_ONLY_COMMANDS = frozenset(
+    [
+        SonyPSXCode.BS_7,
+        SonyPSXCode.BS_11,
+        SonyPSXCode.G_GUIDE,
+    ]
+)
+
+_P002J_ONLY_COMMANDS = frozenset(
+    [
+        SonyPSXCode.G_GUIDE2,
+        SonyPSXCode.FLASH_FORW,
+        SonyPSXCode.FLASH_BACK,
+    ]
+)
+
 # Model variants: earlier (RMT-P001) and later (RMT-P002J) PSX models.
-# Each grouped by switch position.
 # Earlier models include only common codes and P001-exclusive codes.
 # Later models include common codes and P002J-exclusive codes.
-PSX_EARLIER_MODELS_SWITCH_1 = PSXModel(
+PSX_EARLIER_MODELS = PSXModel(
     name=(
-        "PSX earlier models (DESR-5000, DESR-7000, DESR-5100, and DESR-7100), switch 1"
+        "PSX earlier models (DESR-5000, DESR-7000, DESR-5100, and DESR-7100)"
     ),
-    codes=frozenset(
-        code
-        for code in SonyPSXSwitch1Code
-        if code.value[1] not in (_P002J_ONLY_COMMANDS)
-    ),
+    codes=frozenset(code for code in SonyPSXCode if code not in _P002J_ONLY_COMMANDS),
 )
 
-PSX_EARLIER_MODELS_SWITCH_2 = PSXModel(
+PSX_LATER_MODELS = PSXModel(
     name=(
-        "PSX earlier models (DESR-5000, DESR-7000, DESR-5100, and DESR-7100), switch 2"
+        "PSX later models (DESR-5500, DESR-7500, DESR-5700, and DESR-7700)"
     ),
-    codes=frozenset(
-        code
-        for code in SonyPSXSwitch2Code
-        if code.value[1] not in (_P002J_ONLY_COMMANDS)
-    ),
-)
-
-PSX_EARLIER_MODELS_SWITCH_3 = PSXModel(
-    name=(
-        "PSX earlier models (DESR-5000, DESR-7000, DESR-5100, and DESR-7100), switch 3"
-    ),
-    codes=frozenset(
-        code
-        for code in SonyPSXSwitch3Code
-        if code.value[1] not in (_P002J_ONLY_COMMANDS)
-    ),
-)
-
-PSX_LATER_MODELS_SWITCH_1 = PSXModel(
-    name=(
-        "PSX later models (DESR-5500, DESR-7500, DESR-5700, and DESR-7700), switch 1"
-    ),
-    codes=frozenset(
-        code
-        for code in SonyPSXSwitch1Code
-        if code.value[1] not in (_P001_ONLY_COMMANDS)
-    ),
-)
-
-PSX_LATER_MODELS_SWITCH_2 = PSXModel(
-    name=(
-        "PSX later models (DESR-5500, DESR-7500, DESR-5700, and DESR-7700), switch 2"
-    ),
-    codes=frozenset(
-        code
-        for code in SonyPSXSwitch2Code
-        if code.value[1] not in (_P001_ONLY_COMMANDS)
-    ),
-)
-
-PSX_LATER_MODELS_SWITCH_3 = PSXModel(
-    name=(
-        "PSX later models (DESR-5500, DESR-7500, DESR-5700, and DESR-7700), switch 3"
-    ),
-    codes=frozenset(
-        code
-        for code in SonyPSXSwitch3Code
-        if code.value[1] not in (_P001_ONLY_COMMANDS)
-    ),
+    codes=frozenset(code for code in SonyPSXCode if code not in _P001_ONLY_COMMANDS),
 )
