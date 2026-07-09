04af02e5b2d89ae1f5a76d9e5ea3c9d514c788fa
Manu <tr4nt0r@uber.space>
2026-07-10 01:41:16 +0200

    feat!: move Tween Light to generic (#79)

---
+++ infrared_protocols/codes/generic/led/__init__.py
@@ -0,0 +1,6 @@
+"""Generic LED remote control IR command codes."""
+
+from .generic_13_key import Generic13KeyCode
+from .generic_24_key import Generic24KeyCode
+
+__all__ = ["Generic13KeyCode", "Generic24KeyCode"]
