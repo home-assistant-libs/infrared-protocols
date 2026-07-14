04ae6718ea6df465f957d354ec7928d0d07d7fa7
tr4nt0r <tr4nt0r@uber.space>
2026-07-14 18:54:28 +0200

    Add codes for generic 40-key remote

--- infrared_protocols/codes/generic/led/__init__.py
+++ infrared_protocols/codes/generic/led/__init__.py
@@ -2,5 +2,6 @@
 
 from .generic_13_key import Generic13KeyCode
 from .generic_24_key import Generic24KeyCode
+from .generic_40_key import Generic40KeyCode
 
-__all__ = ["Generic13KeyCode", "Generic24KeyCode"]
+__all__ = ["Generic13KeyCode", "Generic24KeyCode", "Generic40KeyCode"]
