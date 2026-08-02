c7ee9a4a5338f929b8edbd0e51424f5cc195b5a1
tr4nt0r <tr4nt0r@uber.space>
2026-08-02 23:57:37 +0200

    fix exports

--- infrared_protocols/codes/generic/led/__init__.py
+++ infrared_protocols/codes/generic/led/__init__.py
@@ -1,6 +1,7 @@
 """Generic LED remote control IR command codes."""
 
 from .base import BaseGenericLEDCode
+from .generic_10_key import Generic10KeyCode
 from .generic_13_key import Generic13KeyCode
 from .generic_24_key import Generic24KeyCode
 from .generic_40_key import Generic40KeyCode
@@ -8,6 +9,7 @@ from .generic_44_key import Generic44KeyCode
 
 __all__ = [
     "BaseGenericLEDCode",
+    "Generic10KeyCode",
     "Generic13KeyCode",
     "Generic24KeyCode",
     "Generic40KeyCode",
